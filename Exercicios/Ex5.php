012218923ad5c77a2168397c9889cbbc97b022f1
Simão Vilaça <simaov.13@gmail.com>
2020-03-09 17:51:58 +0000

    Create Ex5.php

---
+++ Exercicios/Ex5.php
@@ -0,0 +1,15 @@
+<?php
+    $around = "around";
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ex5</title>
+</head>
+<body>
+    <p><?php echo "What goes ". $around. " comes ". $around ?></p>
+</body>
+</html>
