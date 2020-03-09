7fe0dee2e41812b83ba2acf3f875c72f368de22e
Simão Vilaça <simaov.13@gmail.com>
2020-03-09 17:51:48 +0000

    Create Ex3.php

---
+++ Exercicios/Ex3.php
@@ -0,0 +1,19 @@
+<?php
+    $x=10;
+    $y=7;
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ex3</title>
+</head>
+<body>
+    <p><?php echo ($x + $y)?></p>
+    <p><?php echo ($x - $y)?></p>
+    <p><?php echo ($x * $y)?></p>
+    <p><?php echo ($x / $y)?></p>  
+</body>
+</html>
