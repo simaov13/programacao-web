99f69c20e30efb1b139baec5630e3749bf82037b
Simão Vilaça <simaov.13@gmail.com>
2020-03-09 17:51:43 +0000

    Update Ex2.php

---
+++ Exercicios/Ex2.php
@@ -0,0 +1,19 @@
+<?php
+        $hello = "Goodbye ";
+        $world = "World!";
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ex2</title>
+</head>
+<body>
+    <?php
+        echo $hello;
+        echo $world;
+    ?>    
+</body>
+</html>
