a3c8f12877c4672ea8560c35859b60786c907f7d
Simão Vilaça <43792887+simaov13@users.noreply.github.com>
2020-03-30 17:56:30 +0100

    ex3

--- Aula13e14/Exercicios/Ex3/index.php
+++ Aula13e14/Exercicios/Ex3/index.php
@@ -1,3 +1,43 @@
+<?php
+
+    class calculadora {
+        public $altura;
+        public $peso;
+
+        function setAltura($altura){
+            $this->altura = $altura;
+        }
+
+        function setPeso($peso){
+            $this->peso = $peso;
+        }
+
+        function bmi(){
+            $bmi=($this->peso / ($this->altura=$this->altura));
+            return $bmi;
+        }
+    }
+
+    if (isset($_GET["altura"])){
+        $alturaget = $_GET["altura"];
+    }else {
+        $alturaget = "";
+    }
+
+    if (isset($_GET["peso"])){
+        $pesoget = $_GET["peso"];
+    }else {
+        $pesoget = "";
+    }
+
+    if ($alturaget != "" && $pesoget != ""){
+        $pessoa = new calculadora();
+        $pessoa->setAltura($alturaget);
+        $pessoa->setPeso($pesoget);
+        echo $pessoa->bmi();
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="pt">
 <head>
