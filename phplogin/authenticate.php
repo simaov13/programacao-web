a8a04ff8ee73f26b6334f8f2994ab1dac8f743f7
Simão Vilaça <43792887+simaov13@users.noreply.github.com>
2020-05-18 17:37:36 +0100

    loginphp

---
+++ phplogin/authenticate.php
@@ -0,0 +1,77 @@
+<?php
+session_start();
+
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'accounts';
+
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+if ( !isset($_POST['username'], $_POST['password']) ) {
+	exit('Please fill both the username and password fields!');
+}
+
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+	$stmt->bind_param('s', $_POST['username']);
+	$stmt->execute();
+    $stmt->store_result();
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($id, $password);
+        $stmt->fetch();
+        if (password_verify($_POST['password'], $password)) {
+            session_regenerate_id();
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['name'] = $_POST['username'];
+            $_SESSION['id'] = $id;
+            header('Location: home.php');
+        } else {
+            echo 'Incorrect password!';
+        }
+    } else {
+        echo 'Incorrect username!';
+    }
+
+	$stmt->close();
+}
+?>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Profile Page</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	</head>
+	<body class="loggedin">
+		<nav class="navtop">
+			<div>
+				<h1>Website Title</h1>
+				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+			</div>
+		</nav>
+		<div class="content">
+			<h2>Profile Page</h2>
+			<div>
+				<p>Your account details are below:</p>
+				<table>
+					<tr>
+						<td>Username:</td>
+						<td><?=$_SESSION['name']?></td>
+					</tr>
+					<tr>
+						<td>Password:</td>
+						<td><?=$password?></td>
+					</tr>
+					<tr>
+						<td>Email:</td>
+						<td><?=$email?></td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ phplogin/profile.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: index.html');
+	exit;
+}
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'phplogin';
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+$stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
+$stmt->bind_param('i', $_SESSION['id']);
+$stmt->execute();
+$stmt->bind_result($password, $email);
+$stmt->fetch();
+$stmt->close();
+?>
